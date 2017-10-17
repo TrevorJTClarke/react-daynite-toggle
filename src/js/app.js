e5b89f42341bd1c97fe24633ef08a4e570812f8b
Trevor Clarke <besnoid@gmail.com>
2017-10-17 14:22:43 -0700

    Setup base react & webpack to build out toggle

---
+++ src/js/app.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Toggle from './toggle';
+
+import '../scss/main.scss'; // Import CSS
+import mockImg from '../assets/switch_02.gif';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Day/Nite Toggle</h1>
+
+        <div className="examples">
+          <div className="example example-fake">
+            <img src={ mockImg } alt='Yep, This is a mock only' />
+          </div>
+          <div className="example example-toggle">
+            <Toggle />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
---
+++ src/js/toggle.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import '../scss/toggle.scss'; // Import CSS
+
+export default class Toggle extends Component {
+  render() {
+    return (
+      <div className="toggle">
+
+      </div>
+    );
+  }
+}
