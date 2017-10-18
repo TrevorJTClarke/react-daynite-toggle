e7d235adae9b6e5e0f1609330c1d3d3105fce5e1
Trevor Clarke <besnoid@gmail.com>
2017-10-17 17:33:00 -0700

    added built files for demos

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const autoprefixer = require('autoprefixer');
 
 // Constant with our paths
 const paths = {
-  DIST: path.resolve(__dirname, 'dist'),
+  DIST: path.resolve(__dirname, 'docs'),
   SRC: path.resolve(__dirname, 'src'),
   JS: path.resolve(__dirname, 'src/js'),
 };
